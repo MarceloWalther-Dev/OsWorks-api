9b50137ca966b343af16bbd5811b79d495806b80
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2021-03-12 15:56:19 -0300

    Create repository for find client

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,19 +3,23 @@ package com.algaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+	@Autowired
+	ClienteRepository repository;
+	
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente = new Cliente(1L,"Joao","1111111","joãogmail.com");
-		var cliente2 = new Cliente(2L,"Maria","2222222", "mariagmail.com");
-		return Arrays.asList(cliente,cliente2);
+		return repository.findAll();
 	}
 	
 }
