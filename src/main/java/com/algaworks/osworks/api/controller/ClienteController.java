76defc9bf24c5d068221683becf529145f7ef96f
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2021-03-12 16:34:07 -0300

    Post Cliente

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,25 +1,46 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 
+
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	@Autowired
 	ClienteRepository repository;
 	
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return repository.findAll();
+	}
+	
+	@GetMapping("/clienteID")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteID) {
+		var cliente = repository.findById(clienteID).get();
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Cliente> adiciona(@RequestBody Cliente cliente){
+		repository.save(cliente);
+		return ResponseEntity.ok(cliente);
 	}
 	
 }
